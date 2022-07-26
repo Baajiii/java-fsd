8aa6425bf2c56649ccc085f8971b4cb4e0728199
Baajiii <samsudeenbasith00@gmail.com>
2022-07-26 17:59:12 +0530

    practices:

---
+++ phase 1 - practices project-4/src/LongestSubsequences/MyClass.java
@@ -0,0 +1,26 @@
+package LongestSubsequences;
+
+public class MyClass {
+	static int method(int a[], int len) {
+		int s[] = new int[len];
+		int i;
+		int j;
+		int max = 0;
+		for (i = 0; i < len; i++)
+			s[i] = 1;
+		for (i = 1; i < len; i++)
+			for (j = 0; j < i; j++)
+				if (a[i] > a[j] && s[i] < s[j] + 1)
+					s[i] = s[j] + 1;
+		for (i = 0; i < len; i++)
+			if (max < s[i])
+				max = s[i];
+		return max;
+	}
+
+	public static void main(String args[]) {
+		int a[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+		int arr = a.length;
+		System.out.println("The length of the longest increasing subsequence is " + method(a, arr));
+	}
+}
