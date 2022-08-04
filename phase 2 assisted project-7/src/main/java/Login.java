df29b1ced44c409ec17ef7a65be39047fe07af71
Baajiii <samsudeenbasith00@gmail.com>
2022-08-04 15:21:33 +0530

    practice

---
+++ phase 2 assisted project-7/src/main/java/Login.java
@@ -0,0 +1,31 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Login extends GenericServlet {
+
+	public String userid, password;
+
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		userid = req.getParameter("id");
+		password = req.getParameter("password");
+
+		PrintWriter out = res.getWriter();
+		if((userid.equals("baaji")) && (password.equals("pass"))){
+			out.println("Login successfully");
+			out.println("<h4><a href=\"Welcome.html\">Home</a></h4>");
+		} else {
+			out.println("Invalid User Check your Userid and Password");
+			out.println("<h4><a href=\"Login.html\">Go back</a></h4>");
+		}
+
+	}
+
+}
